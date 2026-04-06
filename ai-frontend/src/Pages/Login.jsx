79045ebb36672d0e42e6c26c106b1b1edda37e11
Mutanu207 <mutanumbuvi8@gmail.com>
2026-04-06 16:14:05 +0300

    complete landing page ,with animation and react router code

---
+++ ai-frontend/src/Pages/Login.jsx
@@ -0,0 +1,9 @@
+function Login() {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <h1>Login Page</h1>
+        </div>
+    );
+}
+
+export default Login;
