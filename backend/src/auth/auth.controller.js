c5bf8ecb01b91cacd20b61c760436979558b2c95
Mutanu207 <mutanumbuvi8@gmail.com>
2026-04-25 15:44:55 +0300

    did the backend code for the login route, will add the jwt system authentication next time

--- backend/src/auth/auth.controller.js
+++ backend/src/auth/auth.controller.js
@@ -1,14 +1,29 @@
 import registerUser from "./auth.service.js";
+import loginUser from "./auth.service.js";
 export const register = async (req, res) => {
     try {
         const { email, password } = req.body;
         if (!email || !password) {
             return res.status(400).json({ message: "Email and password are required" });
         } //validation to make sure that email and password has values//
-        const userDetails = await registerUser({ email, password });
+        const userDetails = await registerUser({ email, password }); //What we get back is user id and email//
         res.status(201).json({ message: "User registered successfully", userDetails });
     } catch (error) {
         console.error("Error in register controller:", error);
         res.status(500).json({ message: "Internal server error" });
     }
 };
+export const login= async (req, res) => {
+    try{
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ message: "Email and password are required" });
+        } //validation to make sure that email and password has values//
+        const userEmail = await loginUser({ email, password }); //we get back the email of the user that logged in//
+        res.status(200).json({ message: "User logged in successfully", userEmail });
+
+    } catch (error) {
+        console.error("Error in login controller:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
--- backend/src/auth/auth.service.js
+++ backend/src/auth/auth.service.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import findUserByEmail from "./auth.model.js";
 import createUser from "./auth.model.js"
+import findExistingUser from "./auth.model.js";
 export const registerUser = async ({ email, password }) => {
     try{
         const existingUser = await findUserByEmail(email);
@@ -8,8 +9,20 @@ export const registerUser = async ({ email, password }) => {
         const hashedPassword = await bcrypt.hash(password, 10);
         //we need to pass email,and password to a model.js function to create new user//
         const user= await createUser(email, hashedPassword);
-        return user;
+        return user; //id and email to be returned//
     }
     catch{
         throw new error("Error registering user");  
     }};
+    export const loginUser= async ({email,password}) => {
+        try{
+            const loginUser= await findExistingUser(email); //fetches the user details//
+            if (!loginUser) throw new error("User not found"); //if the user details does exist give error so that user registers//
+            const isMatch = await bcrypt.compare(password, loginUser.password_hash);
+            if (!isMatch) throw new error("Invalid credentials");
+            return loginUser.email; //we can return email //
+        }
+        catch(error){
+            throw new error("Error logging in user");
+        }
+    }
