df952808183d508df98989ee60a7cb65dd871a24
Mutanu207 <mutanumbuvi8@gmail.com>
2026-06-03 12:11:18 +0300

    add axios interceptors to run refresh-token api, so as to get a new acess token automatically after acess token expires, did code for acess token and refresh token

--- backend/src/auth/auth.controller.js
+++ backend/src/auth/auth.controller.js
@@ -54,3 +54,16 @@ export const refreshAccessToken = (req, res) => {
             console.error("Error refreshing access token:", error);
             res.status(401).json({ message: "Invalid refresh token" });
         }}
+    export const logout = (req, res) => {
+        try{
+            res.clearCookie("refreshToken", {
+                httpOnly: true,
+                secure: false,
+                sameSite: "strict",
+            });
+            res.status(200).json({ message: "User logged out successfully" });}
+            catch(error){
+                console.error("Logout in error route:", error)
+                 return res.status(500).json({ message: "Internal server error" });
+
+        }}
--- backend/src/auth/auth.route.js
+++ backend/src/auth/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { register, login, refreshAccessToken } from "./auth.controller.js";
+import { register, login, refreshAccessToken, logout } from "./auth.controller.js";
 import { verifyToken } from "../middleware/auth.middleware.js";
 const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post("/refresh-token", refreshAccessToken);
+router.post("/logout", logout);
 export default router;
